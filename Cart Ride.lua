c4a69e4f2a46af0894e3e6ad1cafb3b44eb732a1
JustDev <89351608+JustDevT@users.noreply.github.com>
2022-12-09 23:02:14 +0300

    Update Cart Ride.lua

--- Cart Ride.lua
+++ Cart Ride.lua
@@ -21,7 +21,7 @@ local Window = Rayfield:CreateWindow({
 		FileName = "DevKey",
 		SaveKey = false,
 		GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-		Key = "nomadcityupdateywz6x"
+		Key = "FjojQa294jmUND8Sr"
 	}
 })
 
