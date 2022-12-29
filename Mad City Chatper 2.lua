5e323f3c46c378ab7130d9dc8ba5abde75abdc3b
JustDev <89351608+JustDevT@users.noreply.github.com>
2022-12-29 21:19:21 +0300

    Update Mad City Chatper 2.lua

--- Mad City Chatper 2.lua
+++ Mad City Chatper 2.lua
@@ -21,7 +21,7 @@ local Window = Rayfield:CreateWindow({
 		FileName = "DevKey",
 		SaveKey = false,
 		GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-		Key = "F466I8hJgz2W"
+		Key = "10K8GpZyXwSF"
 	}
 })
 
