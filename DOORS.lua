ae9d42db90107d2179d6911486b67af01a26e835
JustDev <89351608+JustDevT@users.noreply.github.com>
2023-01-07 20:57:17 +0300

    Update DOORS.lua

--- DOORS.lua
+++ DOORS.lua
@@ -21,7 +21,7 @@ local Window = Rayfield:CreateWindow({
 		FileName = "DevKey",
 		SaveKey = false,
 		GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-		Key = "10K8GpZyXwSF"
+		Key = "S1nvL94E5AY8g9t"
 	}
 })
 
